45ef3754d94778e3baf1b4ef349c4052768b9ed8
laptop <abc1870819884@gmail.com>
2023-02-26 09:34:37 +0000

    ask imgtp

--- frontend/src/views/Ask.vue
+++ frontend/src/views/Ask.vue
@@ -38,13 +38,14 @@ const imgnum = ref(0);
 const imgAdd = (pos, file) => {
     console.log(file);
     const formData = new FormData()
-    const time = Date.now()
-    formData.append("fileName", store.state.user.name + "_" + time + '.' + file.name.split('.').pop());
-    formData.append('image', file)
-
-    // 使用 axios 等工具发送图片到服务端
-    axios.post('https://www.codehelp.cn:3000/api/upload2', formData).then(({ data }) => {
-        const url = data.url
+formData.append("image", file);
+    axios.post('https://www.imgtp.com/api/upload', formData, {
+        headers: {
+            'token': '1fee373d94bf7bf2b87fcbf756b716d2',
+            'content-type': 'multipart/form-data'
+        }
+    }).then(({ data }) => {
+        const url = data.data.url;
         content.value = content.value.replace(/!\[[^\]]+\]\([^)]+\)/, `![](${url})`);
         imgnum.value++;
     })
